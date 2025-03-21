828f18e2d06f2b9c2a98407692c65b60da232dc7
JizzDev1 <jizzdev1@gmail.com>
2025-03-21 11:41:21 +0100

    simplified grammar to bust nuts more easily

--- src/parser/ast.h
+++ src/parser/ast.h
@@ -2,176 +2,198 @@
 #define AST
 
 typedef enum {
-	NODEPROGRAM, NODESTATEMENT, NODEOUTPUT, NODEIF, NODECASE, NODELOOP, NODERETURN, NODEEXIT, NODEFUNCTIONDECL, NODEINPUT, NODEDECLARATION, NODEASSIGNMENT,
-	NODEFUNCTIONCALL, NODEEXPRESSION, NODELOGOR, NODELOGAND, NODEBITOR, NODEBITAND, NODEEQUALITY, NODEINEQ, NODEADD, NODEMULT, NODEPOWER, NODENOT,
-	NODEPRIMARY
+	NONE,
+	PROGRAM,
+	// statements
+	OUTPUT,
+	IF,
+	CASE,
+	LOOP,
+	RETURN,
+	EXIT,
+	FUNCDECL,
+	INPUT,
+	DECL,
+	ASSIGN,
+	FUNCALL,
+	ELSE,
+	ELSECASE,
+	PARAMS,
+	// expressions
+	LOGOR,
+	LOGAND,
+	BITOR,
+	BITAND,
+	EQ,
+	NEQ,
+	GT,
+	GE,
+	LT,
+	LE,
+	ADD,
+	SUB,
+	CONCAT,
+	MULT,
+	DIV,
+	MODULO,
+	// primary data
+	NATURAL,
+	NEGINT,
+	POSREAL,
+	NEGREAL,
+	IDENTIF,
+	STRING
 } NodeType;
 
 typedef struct {
-	NodeType node_type;
-	NodeType child_type;
-	void* statement; // Output* | If* | Case* | Loop* | Return* | Exit* | FunctionDecl* | Input* | Declaration* | Assignment* | FunctionCall* | Expression*
-} NodeStatement;
+	NodeType nodetype; // assign PROGRAM
+	NodeType statement_type; // NONE | OUTPUT | IF | CASE | LOOP | RETURN | EXIT | FUNCDECL | INPUT | DECL | ASSIGN | FUNCALL
+	void* statement; // statement; can be NULL or one of the follwing node pointers
+	// OutputNode* | IfNode* | CaseNode*
+	// LoopNode* | ReturnNode* | ExitNode* |
+	// FuncDeclNode* | InputNode* | DeclNode* |
+	// AssingNode* | FunCallNode*
+} ProgramNode;
 
 typedef struct {
-	NodeType node_type;
-	NodeStatement** statements;
-} NodeProgram;
-
-typedef struct {
-	NodeType node_type;
-	void* expr; // NodeExpression*
-	Token* primary_datatype;
-} NodePrimary;
-
-typedef struct _NodeNot_ {
-	NodeType node_type;
-	struct _NodeNot_* recnot;
-	NodePrimary* primary;
-} NodeNot;
-
-typedef struct _NodePower_ {
-	NodeType node_type;
-	NodeNot* operand;
-	struct _NodePower_* exponent;
-} NodePower;
-
-typedef struct {
-	NodeType node_type;
-	Token* operation;
-	NodePower* operand1;
-	NodePower* operand2;
-} NodeMult;
-
-typedef struct {
-	NodeType node_type;
-	Token* operation;
-	NodeMult* operand1;
-	NodeMult* operand2;
-} NodeAdd;
-
-typedef struct {
-	NodeType node_type;
-	Token* operation;
-	NodeAdd* operand1;
-	NodeAdd* operand2;
-} NodeIneq;
-
-typedef struct {
-	NodeType node_type;
-	Token* operation;
-	NodeIneq* operand1;
-	NodeIneq* operand2;
-} NodeEquality;
-
-typedef struct {
-	NodeType node_type;
-	NodeEquality* operand1;
-	NodeEquality* operand2;
-} NodeBitAnd;
-
-typedef struct {
-	NodeType node_type;
-	NodeBitAnd* operand1;
-	NodeBitAnd* operand2;
-} NodeBitOr;
-
-typedef struct {
-	NodeType node_type;
-	NodeBitOr* operand1;
-	NodeBitOr* operand2;
-} NodeLogAnd;
-
-typedef struct {
-	NodeType node_type;
-	NodeLogAnd* operand1;
-	NodeLogAnd* operand2;
-} NodeLogOr;
-
-typedef struct {
-	NodeType node_type;
-	NodeLogOr* logical_or;
-} NodeExpression;
-
-typedef struct {
-	NodeType node_type;
-	Token* function_name;
-	NodeExpression** args;
-} NodeFunctionCall;
-
-typedef struct {
-	NodeType node_type;
-	Token* var;
-	NodeExpression* expr;
-} NodeAssignment;
-
-typedef struct {
-	NodeType node_type;
-	Token* data_type;
-	Token* var;
-	NodeAssignment* assn;
-} NodeDeclaration;
-
-typedef struct {
-	NodeType node_type;
-	Token* data_type;
-	Token* var;
-	Token* inputstr;
-} NodeInput;
-
-typedef struct {
-	NodeType node_type;
-	Token* function_name;
-	Token* args_types;
-	Token* args_names;
-	NodeProgram* procedure;
-} NodeFunctionDecl;
-
-typedef struct {
-	NodeType node_type;
-	Token* nat_token;
-} NodeExit;
-
-typedef struct {
-	NodeType node_type;
-	NodeExpression* return_expr;
-} NodeReturn;
-
-typedef struct {
-	NodeType node_type;
-	NodeExpression* condition;
-
-	NodeType loop_instructions_type;
-	void* loop_instructions; // NodeStatement* | NodeProgram*
-} NodeLoop;
-
-typedef struct {
-	NodeType node_type;
-	NodeExpression* condition;
-
-	NodeType mainbrach_type;
-	void* mainbranch; // NodeStatement* | NodeProgram*
-
-	NodeType elsecasebrach_type;
-	void* elsecasebranch; // NodeStatement* | NodeProgram*
-} NodeCase;
-
-typedef struct {
-	NodeType node_type;
-	NodeExpression* condition;
-
-	NodeType mainbranch_type;
-	void* mainbranch; // NodeStatement* | NodeProgram*
+	NodeType nodetype; // assign OUTPUT
 	
-	NodeType elsebranch_type;
-	void* elsebranch; // NodeStatement* | NodeProgram*
-} NodeIf;
+	NodeType output_content_type;
+	void* output_content; // expression; can be one of the following node pointers
+	// LogOrNode* | LogAndNode* |
+	// BitOrNode* | BitAndNode* |
+	// EqNode* | NeqNode*| GtNode* |
+	// GeNode* | LtNode*| LeNode* |
+	// AddNode* | SubNode*| ConcatNode* |
+	// MultNode* | DivNode*| ModuloNode* |
+	// NaturalNode* | NegIntNode* |
+	// PosRealNode* | NegRealNode* |
+	// IdentifNode* | StringNode* |
+	// ElseNode* | ElsecaseNode* | ParamsNode*
+} OutputNode;
 
 typedef struct {
-	NodeType node_type;
-	NodeExpression* expression_node;
-} NodeOutput;
+	NodeType nodetype; // assign IF
+	
+	NodeType condition_type;
+	void* condition; // expression; can be one of the following node pointers
+	// LogOrNode* | LogAndNode* |
+	// BitOrNode* | BitAndNode* |
+	// EqNode* | NeqNode*| GtNode* |
+	// GeNode* | LtNode*| LeNode* |
+	// AddNode* | SubNode*| ConcatNode* |
+	// MultNode* | DivNode*| ModuloNode* |
+	// NaturalNode* | NegIntNode* |
+	// PosRealNode* | NegRealNode* |
+	// IdentifNode* | StringNode* |
+	// ElseNode* | ElsecaseNode* | ParamsNode*
+	
+	ProgramNode* mainbranch;
+	ProgramNode* elsebranch;
+} IfNode;
 
-typedef NodeProgram* AbstractSyntaxTree;
+typedef struct {
+	NodeType nodetype; // assign CASE
+	
+	NodeType condition_type;
+	void* condition; // expression; can be one of the following node pointers
+	// LogOrNode* | LogAndNode* |
+	// BitOrNode* | BitAndNode* |
+	// EqNode* | NeqNode*| GtNode* |
+	// GeNode* | LtNode*| LeNode* |
+	// AddNode* | SubNode*| ConcatNode* |
+	// MultNode* | DivNode*| ModuloNode* |
+	// NaturalNode* | NegIntNode* |
+	// PosRealNode* | NegRealNode* |
+	// IdentifNode* | StringNode* |
+	// ElseNode* | ElsecaseNode* | ParamsNode*
+	
+	ProgramNode* mainbranch;
+	ProgramNode* elsebranch;
+} CaseNode;
+
+typedef struct {
+	NodeType nodetype; // assign LOOP
+	
+	NodeType condition_type;
+	void* condition; // expression; can be one of the following node pointers
+	// LogOrNode* | LogAndNode* |
+	// BitOrNode* | BitAndNode* |
+	// EqNode* | NeqNode*| GtNode* |
+	// GeNode* | LtNode*| LeNode* |
+	// AddNode* | SubNode*| ConcatNode* |
+	// MultNode* | DivNode*| ModuloNode* |
+	// NaturalNode* | NegIntNode* |
+	// PosRealNode* | NegRealNode* |
+	// IdentifNode* | StringNode* |
+	
+	ProgramNode* procedure;
+} LoopNode;
+
+typedef struct {
+	NodeType nodetype; // assign RETURN
+	
+	NodeType exprtype;
+	void* expr; // expression; can be one of the following node pointers
+	// LogOrNode* | LogAndNode* |
+	// BitOrNode* | BitAndNode* |
+	// EqNode* | NeqNode*| GtNode* |
+	// GeNode* | LtNode*| LeNode* |
+	// AddNode* | SubNode*| ConcatNode* |
+	// MultNode* | DivNode*| ModuloNode* |
+	// NaturalNode* | NegIntNode* |
+	// PosRealNode* | NegRealNode* |
+	// IdentifNode* | StringNode* |
+	// ElseNode* | ElsecaseNode* | ParamsNode*
+} ReturnNode;
+
+typedef struct {
+	NodeType nodetype; // assign EXIT
+	Token* natural;
+} ExitNode;
+
+typedef struct {
+	NodeType nodetype; // assign FUNCDECL
+	Token* function_name;
+	ProgramNode* procedure;
+	// put parameters' types and names in symbol table (yet to be implemented)
+} FuncDeclNode;
+
+typedef struct {
+	NodeType nodetype; // assign INPUT
+	Token* storevar;
+	// in case we have an input without declaration, check if variable is in symbol table (yet to be implemented)
+} InputNode;
+
+typedef struct {
+	NodeType nodetype; // assign DECL
+	Token* var;
+	// put variable's data type in symbol table (yet to be implemented)
+} DeclNode;
+
+typedef struct {
+	NodeType nodetype; // assign ASSIGN (lol)
+	Token* varname;
+	// put variable type and content in symbol table (yet to be implemented)
+} AssingNode;
+
+typedef struct {
+	NodeType nodetype; // assign FUNCALL
+	Token* function_name;
+	void** expressions; // use symbol table to check if function exists and expressions' count and types match with existing function's
+	// expressions array can be NULL or contain one or more of the following node pointers
+	// LogOrNode* | LogAndNode* |
+	// BitOrNode* | BitAndNode* |
+	// EqNode* | NeqNode*| GtNode* |
+	// GeNode* | LtNode*| LeNode* |
+	// AddNode* | SubNode*| ConcatNode* |
+	// MultNode* | DivNode*| ModuloNode* |
+	// NaturalNode* | NegIntNode* |
+	// PosRealNode* | NegRealNode* |
+	// IdentifNode* | StringNode* |
+	// ElseNode* | ElsecaseNode* | ParamsNode*
+} FunCallNode;
+
+// add expression nodes
 
 #endif
--- src/parser/parser.h
+++ src/parser/parser.h
@@ -3,30 +3,4 @@
 
 #define MIN_STATEMENTS 1
 
-NodeProgram* parse_program(Token* tokenlist, size_t tokenlist_cursor);
-NodeStatement* parse_statement(Token* tokenlist, size_t* tokenlist_cursor);
-NodeOutput* parse_output(Token* tokenlist, size_t* tokenlist_cursor);
-NodeIf* parse_if(Token* tokenlist, size_t* tokenlist_cursor);
-NodeCase* parse_case(Token* tokenlist, size_t* tokenlist_cursor);
-NodeLoop* parse_loop(Token* tokenlist, size_t* tokenlist_cursor);
-NodeReturn* parse_return(Token* tokenlist, size_t* tokenlist_cursor);
-NodeExit* parse_exit(Token* tokenlist, size_t* tokenlist_cursor);
-NodeFunctionDecl* parse_function_decl(Token* tokenlist, size_t* tokenlist_cursor);
-NodeInput* parse_input(Token* tokenlist, size_t* tokenlist_cursor);
-NodeDeclaration* parse_declaration(Token* tokenlist, size_t* tokenlist_cursor);
-NodeAssignment* parse_assignment(Token* tokenlist, size_t* tokenlist_cursor);
-NodeFunctionCall* parse_function_call(Token* tokenlist, size_t* tokenlist_cursor);
-NodeExpression* parse_expression(Token* tokenlist, size_t* tokenlist_cursor);
-NodeLogOr* parse_logical_or(Token* tokenlist, size_t* tokenlist_cursor);
-NodeLogAnd* parse_logical_and(Token* tokenlist, size_t* tokenlist_cursor);
-NodeBitOr* parse_bitwise_or(Token* tokenlist, size_t* tokenlist_cursor);
-NodeBitAnd* parse_bitwise_and(Token* tokenlist, size_t* tokenlist_cursor);
-NodeEquality* parse_equality(Token* tokenlist, size_t* tokenlist_cursor);
-NodeIneq* parse_inequality(Token* tokenlist, size_t* tokenlist_cursor);
-NodeAdd* parse_addition(Token* tokenlist, size_t* tokenlist_cursor);
-NodeMult* parse_multiplication(Token* tokenlist, size_t* tokenlist_cursor);
-NodePower* parse_power(Token* tokenlist, size_t* tokenlist_cursor);
-NodeNot* parse_negation(Token* tokenlist, size_t* tokenlist_cursor);
-NodePrimary* parse_primary(Token* tokenlist, size_t* tokenlist_cursor);
-
 #endif
